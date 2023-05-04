9fef6dddcfeae5a03044baa22cc48710e1f49405
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-04 09:36:54 +0800

    feat: made changes to navbar links

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,24 @@
 <template>
   <div>
     <div role="navigation">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <img src="./assets/cat_logo.png" alt="cat_logo"> 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <RouterLink class="nav-link active" aria-current="page" to="home">Home</RouterLink>
                         </li>
                         <li class="nav-item">
-                            <RouterLink class="nav-link active" aria-current="page" to="about">About</RouterLink>
+                            <RouterLink class="nav-link" aria-current="page" to="about">About</RouterLink>
                         </li>
                         <li class="nav-item">
-                            <RouterLink class="nav-link active" aria-current="page" to="contactus">Contact Us</RouterLink>
+                            <RouterLink class="nav-link" aria-current="page" to="contactus">Contact Us</RouterLink>
                         </li>  
                     </ul>
                 </div>
@@ -36,5 +36,33 @@
 </template>
 
 <style scoped>
+
+img {
+  width: 45px;
+  margin: 5px 5px;
+}
+
+.nav-link.active {
+  text-decoration: underline;
+  text-decoration-thickness: 4px;
+  text-underline-offset: 5px;
+
+}
+
+li {
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0px 20px;
+}
+
+li:hover {
+  text-decoration: underline;
+  text-decoration-thickness: 4px;
+  text-underline-offset: 5px;
+}
+
+#navbar {
+  background: transparent;
+}
 
 </style>
