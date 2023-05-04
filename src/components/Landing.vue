466e0df857dbbce07e5f2773dbfb4a0a7ec8ab5b
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-04 13:09:21 +0800

    feat: added components

---
+++ src/components/Landing.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>CAT-CHING!</h1>
+        <p>Cat + Kuching, we're a cat adoption shelter aiming to protect, heal and find forever homes for our feline
+            overlords!</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+h1 {
+    font-size: 100px;
+    font-weight: bold;
+    margin-top: 30px;
+    margin-left: 50px;
+}
+
+p {
+    margin-left: 50px;
+}
+
+#header {
+    background-color: brown;
+}
+
+#headerbg {
+    width: 100%;
+    height: auto;
+    background-image: url(../assets/cat02.jpg);
+}
+</style>
