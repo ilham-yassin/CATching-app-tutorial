d7a5b3b6236cd9ee9ff7a28efbbf1977508bb4a0
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-04 13:15:54 +0800

    feat: added palceholder to content and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Footer Here</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+h1 {
+    font-size: 50px;
+    margin-top: 30px;
+    margin-left: 50px;
+}
+
+</style>
