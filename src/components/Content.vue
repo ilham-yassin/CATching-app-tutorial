d998f4ac06c47e0fb6dd1f3babd00c631dc54cb3
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-04 19:01:17 +0800

    feat: added card to page

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,23 +1,28 @@
 <template>
-    <div>
-        <h1>Main Content Here</h1>
+    <div id="headline">
+        <h2>Here Are Some Cats In Our Shelter</h2>
+        <p>Please click on the Adopt A Cat button above to find out their availability.</p>
+        <CatCard  />
     </div>
 </template>
 
 <script setup lang="ts">
-
+    import CatCard from "./CatCard.vue"
 </script>
 
 <style scoped>
 
-h1 {
-    font-size: 50px;
-    margin-top: 30px;
-    margin-left: 50px;
+#headline {
+    text-align: center;
 }
 
-div {
-    background-color:blueviolet;
+h2 {
+    font-size: 30px;
+    margin-top: 70px;
+}
+
+p {
+    font-weight: light;
 }
 
 </style>
