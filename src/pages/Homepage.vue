829025857583c0ba3346e46efe83d2d4eee2bc4d
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-04 13:15:10 +0800

    feat: moved codes from homepage to their own components

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,32 +1,17 @@
 <template>
-    <div class="container">
-        <header>
-            <slot name="header">
-                <h1>Cat-ching!</h1>
-                <p>Cat + Kuching, we're a cat adoption shelter aiming to protect, heal and find forever homes for our feline
-                overlords!</p>
-            </slot>
-        </header>
-        <main>
-            <slot></slot>
-        </main>
-        <footer>
-            <slot name="footer"></slot>
-        </footer>
-    </div>
+        <Landing />
+        <Content />
+        <Footer />
 </template>
 
 <script setup lang="ts">
+    import Landing from "../components/Landing.vue"
+    import Content from "../components/Content.vue"
+    import Footer from "../components/Footer.vue"
 
 </script>
 
-<style scoped>
-h1 {
-    margin-top: 30px;
-    margin-left: 50px;
-}
 
-p {
-    margin-left: 50px;
-}
+<style scoped>
+
 </style>
