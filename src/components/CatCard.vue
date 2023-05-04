8c08e9a10867321bc2c9797fc1799c83c17cda7c
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-04 19:01:37 +0800

    feat: made card for content

---
+++ src/components/CatCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Cat Name</h5>
+            <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+            <a href="#" class="btn btn-primary">Find out More</a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.card {
+    margin: 50px 0px 50px 50px;
+}
+
+</style>
