db8bd8b342f98ec4f367cc634eabeacaa926f488
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-05 08:38:32 +0800

    feat: created card component for the content page

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -1,10 +1,117 @@
 <template>
-    <div class="card" style="width: 18rem;">
+    <!-- <div class="card" style="width: 18rem;">
         <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Cat Name</h5>
             <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
             <a href="#" class="btn btn-primary">Find out More</a>
+        </div>
+    </div>
+
+    <div class="row">
+
+    </div> -->
+
+    <div class="row row-cols-1 row-cols-md-5 g-10">
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Cat Name</h5>
+                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
+                    <a href="#" class="btn btn-primary">Find out More</a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -14,8 +121,16 @@
 </script>
 
 <style scoped>
+
+.col {
+    margin: 25px 0px 25px 50px;
+}
 .card {
-    margin: 50px 0px 50px 50px;
+    margin: 0px 0px 0px 0px;
 }
 
+.btn {
+    background-color: #000;
+    border-color: #000;
+}
 </style>
