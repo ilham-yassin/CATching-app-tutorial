24626d4b73d5d28ef5b1d05dab8ef602ffc86ce2
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-05 11:12:28 +0800

    feat: changed src for images

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -25,7 +25,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -35,7 +36,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -45,7 +47,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -55,7 +58,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -65,7 +69,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -75,7 +80,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -85,27 +91,8 @@
         </div>
         <div class="col">
             <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Cat Name</h5>
-                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
-                    <a href="#" class="btn btn-primary">Find out More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Cat Name</h5>
-                    <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
-                    <a href="#" class="btn btn-primary">Find out More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <img src="../assets/cat01.jpg" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1600&q=60"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Cat Name</h5>
                     <p class="card-text">Here's some info on the cat, maybe you'll like em, maybe not.</p>
@@ -121,10 +108,10 @@
 </script>
 
 <style scoped>
-
 .col {
     margin: 25px 0px 25px 50px;
 }
+
 .card {
     margin: 0px 0px 0px 0px;
 }
@@ -132,5 +119,4 @@
 .btn {
     background-color: #000;
     border-color: #000;
-}
-</style>
+}</style>
