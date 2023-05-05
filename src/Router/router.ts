1d7a64415e88d2c08e3e579798220a8e91899d74
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-05 11:12:00 +0800

    feat: added more pages and their routes

--- src/Router/router.ts
+++ src/Router/router.ts
@@ -2,13 +2,34 @@ import { createWebHistory, createRouter } from 'vue-router'
 import Homepage from '../pages/Homepage.vue'
 import About from '../pages/About.vue'
 import ContactUs from '../pages/ContactUs.vue'
+import Gallery from '../pages/Gallery.vue'
+import Programs from '../pages/Programs.vue'
+import Services from '../pages/Services.vue'
 
 
 export const routes = [
 	{
+		path: '/cats',
+		name: 'cats',
+		component: Gallery,
+	},
+
+	{
 		path: '/home',
 		name: 'home',
 		component: Homepage,
+	},
+
+	{
+		path: '/programs',
+		name: 'programs',
+		component: Programs,
+	},
+
+	{
+		path: '/services',
+		name: 'services',
+		component: Services,
 	},
 
 	{
