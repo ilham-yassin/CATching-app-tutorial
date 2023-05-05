fc808fd5740f6d0cf167587ac055777c34e6af5a
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-05 11:14:47 +0800

    feat: refractor navabr to component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <script setup lang="ts">
 import Footer from "./components/Footer.vue"
+import Navbar from "./components/Navbar.vue"
 
 </script>
 
 <template>
   <div id="mainhome">
-    <div>
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <img src="./assets/cat_logo.png" alt="cat_logo">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <RouterLink class="nav-link" aria-current="page" to="home">Home</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink class="nav-link" aria-current="page" to="about">About</RouterLink>
-              </li>
-              <li class="nav-item">
-                <RouterLink class="nav-link" aria-current="page" to="contactus">Contact Us</RouterLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <slot>
+      <Navbar />
+    </slot>
     <slot>
       <router-view :key="$route.path" />
     </slot>
@@ -39,24 +19,5 @@ import Footer from "./components/Footer.vue"
 </template>
 
 <style scoped>
-img {
-  width: 45px;
-  margin: 5px 5px;
-}
 
-li {
-  font-size: 20px;
-  font-weight: bold;
-  margin: 0px 20px;
-}
-
-li:hover {
-  text-decoration: underline;
-  text-decoration-thickness: 4px;
-  text-underline-offset: 5px;
-}
-
-#navbar {
-  background: transparent;
-}
 </style>
