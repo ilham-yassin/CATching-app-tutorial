7950342aa436cab661094f0cfb55a86066539f8f
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-09 15:32:49 +0800

    feat: added default layout to App

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
-<script setup lang="ts">
-import Footer from "./components/Footer.vue"
-import Navbar from "./components/Navbar.vue"
-
-</script>
-
-<script lang="ts">
-</script>
-
 <template>
-  <div id="mainhome">
-    <slot>
-      <Navbar />
-    </slot>
-    <slot>
-      <router-view :key="$route.path" />
-    </slot>
-  </div>
-  <slot>
-    <Footer />
-  </slot>
+  <DefaultLayout>
+    <RouterView></RouterView>
+  </DefaultLayout>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { DefaultLayout } from './components/layouts';
+
+</script>
---
+++ src/components/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <Navbar></Navbar>
+    <slot></slot>
+    <Footer></Footer>
+</template>
+
+<script setup lang="ts">
+import Navbar from '../Navbar.vue';
+import Footer from '../Footer.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
