3e8020a71ca3b57a6070c138bf80cb1305151bd7
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-09 15:34:06 +0800

    feat: added pinia, imports and exports

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar.vue';
+export { default as CatCard } from './CatCard.vue';
+
+export * from './layouts';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import "./styles/core.scss"
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Router/router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import pinia from "./store"
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
