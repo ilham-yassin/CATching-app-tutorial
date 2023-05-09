2afc7d4f1789ca30a174068edada849f57ef25de
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-09 15:46:31 +0800

    feat: added cat search and call

---
+++ src/components/AddCatSearch.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="onSubmit" class="hstack gap-3">
+        <input type="text"
+            class="form-control"
+            placeholder="Search for Cat Breed"
+            v-model="searchTerms">
+        <button class="btn btn-primary">Search</button>
+    </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useCatsStore } from '../store';
+
+const catsStore = useCatsStore();
+
+const searchTerms = ref('');
+
+const onSubmit = async () => {
+    await catsStore.findAndAdd(searchTerms.value);
+    searchTerms.value = '';
+}
+</script>
+
+<style scoped>
+.btn {
+    background-color: #000;
+    border-color: #000;
+    color:#fff;
+}
+</style>
