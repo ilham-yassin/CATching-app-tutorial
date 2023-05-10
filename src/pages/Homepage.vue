64e08a6ad32b37e010462dae20c2f794d8bbbdf1
ilhamyassin <ilham.hmyassin@gmail.com>
2023-05-10 09:26:20 +0800

    feat: template formatting and div usage

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,21 +1,46 @@
 <template>
-    <slot>
-        <Landing />
-    </slot>
-    <slot>
-        <Content />
-    </slot>
-    <slot>
-        <CallToAction />
-    </slot>
-
+        <div class="container">
+            <Landing></Landing>
+            <div id="headline">
+                <h2>Here Are Some Cat Breeds In Our Shelter</h2>
+                <p>Please click on the Adopt A Cat button above to find out more.</p>
+            </div>
+            <div id="cardlist">
+                <CatCard></CatCard>
+                <AddCatSearch class="py-5"></AddCatSearch>
+            </div>
+            
+        </div>
+        <CallToAction></CallToAction>
 </template>
 
 <script setup lang="ts">
 import Landing from "../components/Landing.vue"
-import Content from "../components/Content.vue"
 import CallToAction from "../components/CallToAction.vue"
+import AddCatSearch from "../components/AddCatSearch.vue"
+import { useCatsStore } from "../store"
+import { CatCard } from "../components"
+
+useCatsStore().load()
 </script>
 
 
-<style scoped></style>
+<style scoped>
+#headline {
+    text-align: center;
+}
+
+#cardlist {
+
+    text-align: center;
+}
+
+h2 {
+    font-size: 30px;
+    margin-top: 100px;
+}
+
+p {
+    font-weight: light;
+}
+</style>
